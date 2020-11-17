5eb8a12b6b888deee80790f90c8ad8b803d2de7f
Lucy May <lucymair@live.co.uk>
2020-11-17 18:22:27 +0000

    Add wip image

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import wip from './assets/work-in-progress.jpeg';
 
 function App() {
-  return <div className="App">Hello Lucy</div>;
+  return (
+    <div className="App">
+      <div>Hello Lucy</div>
+      <img className="wip" src={wip} />
+    </div>
+  );
 }
 
 export default App;
