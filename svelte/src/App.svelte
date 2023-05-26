3399f49a09faf8295ab49a524b509109c9b31a1c
Lucy Mair <lucy.may.mair@outlook.com>
2023-05-26 08:28:19 +0100

    Use static HTML for home

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,8 +1,34 @@
 <script lang="ts">
+  import { writable } from "svelte/store";
   import { Scene } from "./components";
   import { loadIntroScene } from "./scenes/intro-scene";
 
   const source = loadIntroScene();
+  const pathnameStore = writable<string>(window.location.pathname);
+
+  const foo = document.querySelector("#foo");
+  const html = (foo?.innerHTML ?? "Unknown").replace("HTML", "Svelte");
+
+  foo && foo.setAttribute("style", "display: none");
+
+  let testDiv: HTMLDivElement;
+
+  $: {
+    if (testDiv) {
+      testDiv.innerHTML = html;
+    }
+  }
+
+  const navigate = (pathname: string) => {
+    window.history.pushState("", "", pathname);
+    pathnameStore.set(window.location.pathname);
+  };
 </script>
 
-<Scene {source} />
+{#if $pathnameStore === "/scene"}
+  <button on:click={() => navigate("/")}>Home</button>
+  <Scene {source} />
+{:else}
+  <div bind:this={testDiv} />
+  <button on:click={() => navigate("/scene")}>Go to scene</button>
+{/if}
