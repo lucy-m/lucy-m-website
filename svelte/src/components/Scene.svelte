2f8b2d519b233e12db7acdacfd6643d2d0870cde
Lucy Mair <lucy.may.mair@outlook.com>
2023-05-27 23:16:09 +0100

    Add loading animation

--- svelte/src/components/Scene.svelte
+++ svelte/src/components/Scene.svelte
@@ -14,7 +14,12 @@
   style:height="{canvasHeight}px"
 >
   {#await loadScene(source)}
-    <p>Loading</p>
+    <div class="loading">
+      <p>Loading</p>
+      <p>.</p>
+      <p>.</p>
+      <p>.</p>
+    </div>
   {:then scene}
     <SceneViewer {scene} {canvasHeight} {canvasWidth} />
   {/await}
@@ -29,5 +34,38 @@
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+  .loading {
+    font-size: 2rem;
+    display: flex;
+  }
+
+  .loading > :nth-child(n + 2) {
+    animation-name: hop;
+    animation-duration: 1200ms;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+
+  .loading > :nth-child(2) {
+    animation-delay: 500ms;
+  }
+
+  .loading > :nth-child(3) {
+    animation-delay: 650ms;
+  }
+
+  .loading > :nth-child(4) {
+    animation-delay: 800ms;
+  }
+
+  @keyframes hop {
+    85% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-0.5rem);
+    }
   }
 </style>
