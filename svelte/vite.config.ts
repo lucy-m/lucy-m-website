41116d791d4e5a995d7e5c761f6d94fce6559316
Lucy Mair <lucy.may.mair@outlook.com>
2023-05-27 23:27:43 +0100

    Add build timestamp

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -1,8 +1,23 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import filterReplaceBase from "vite-plugin-filter-replace";
+
+const filterReplace = (filterReplaceBase as any)
+  .default as typeof filterReplaceBase;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    filterReplace([
+      {
+        filter: "index.html",
+        replace: {
+          from: "__BUILD_TIMESTAMP__",
+          to: new Date().toISOString(),
+        },
+      },
+    ]),
+    svelte(),
+  ],
   assetsInclude: ["**/*.PNG"],
 });
