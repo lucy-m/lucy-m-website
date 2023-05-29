c56adbe61e3ebabf39a73f7da90ec073e8f460f8
Lucy Mair <lucy.may.mair@outlook.com>
2023-05-29 22:28:52 +0100

    Load images quicker if seen before

--- svelte/src/model/throttle-layers.ts
+++ svelte/src/model/throttle-layers.ts
@@ -1,14 +1,45 @@
-import { from, map, of, switchMap, timer, zip, type Observable } from "rxjs";
+import { concat, concatMap, delay, of, type Observable } from "rxjs";
 import type { Layer } from "./layer";
 
 export const throttleLayers = <TLayerKey>(
   images: Layer<TLayerKey>[]
 ): Observable<Layer<TLayerKey>> => {
-  const baseDataSource: Observable<Layer<TLayerKey>> = of(images).pipe(
-    switchMap((images) => {
-      return zip(from(images), timer(0, 50)).pipe(map(([image]) => image));
-    })
-  );
+  const baseTime = 200;
+  const timeDecrement = 100;
+  const minTime = 20;
+
+  interface Accumulator {
+    imageDelays: Map<HTMLImageElement, number>;
+    throttled: Observable<Layer<TLayerKey>>[];
+  }
+
+  const initial: Accumulator = {
+    imageDelays: new Map(),
+    throttled: [],
+  };
+
+  const throttled = images.reduce<Accumulator>((acc, next) => {
+    if (next.content.kind === "image") {
+      const imageDelay = Math.max(
+        (acc.imageDelays.get(next.content.image) ?? baseTime) - timeDecrement,
+        minTime
+      );
+
+      const imageDelays = acc.imageDelays.set(next.content.image, imageDelay);
+      const throttled = [...acc.throttled, of(next).pipe(delay(imageDelay))];
+
+      return { imageDelays, throttled };
+    } else {
+      return {
+        imageDelays: acc.imageDelays,
+        throttled: [...acc.throttled, of(next).pipe(delay(baseTime))],
+      };
+    }
+  }, initial);
+
+  const baseDataSource: Observable<Layer<TLayerKey>> = concat(
+    throttled.throttled
+  ).pipe(concatMap((v) => v));
 
   return baseDataSource;
 };
