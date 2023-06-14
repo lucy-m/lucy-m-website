f9ea0c458cc152002941dbd667da4e526aa5624c
Lucy Mair <lucy.may.mair@outlook.com>
2023-06-14 17:12:46 +0100

    Add a very important animation

--- svelte/src/main.ts
+++ svelte/src/main.ts
@@ -6,16 +6,26 @@ const app = new App({
 
 export default app;
 
-const style = [
-  "color: mediumslateblue",
-  "font-family: 'Comic Sans MS'",
-  "font-size: 16px",
-  "line-height: 22px",
-].join(";");
+if (window.location.hostname !== "localhost") {
+  const style = [
+    "color: mediumslateblue",
+    "font-family: 'Comic Sans MS'",
+    "font-size: 16px",
+    "line-height: 22px",
+  ].join(";");
 
-console.log(
-  "%cHey friend!\nI assume you're a fellow developer and you want to have a nose at my code. " +
-    "Luckily for you, this website is open source so you can see it in all its unminified glory " +
-    "at https://github.com/lucy-m/lucy-m-website.",
-  style
-);
+  console.log(
+    "%cHey friend!\nI assume you're a fellow developer and you want to have a nose at my code. " +
+      "Luckily for you, this website is open source so you can see it in all its unminified glory " +
+      "at https://github.com/lucy-m/lucy-m-website.",
+    style
+  );
+}
+
+document.querySelectorAll(".hover-wave").forEach((hoverWave) => {
+  const letters = Array.from(hoverWave.innerHTML);
+  const wrappedLetters = letters.map(
+    (letter, i) => `<span style="animation-delay: ${i * 50}ms">${letter}</span>`
+  );
+  hoverWave.innerHTML = wrappedLetters.join("");
+});
