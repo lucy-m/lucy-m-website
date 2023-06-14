34711c69bfcdf7d0815a51e468ec452cc9990753
Lucy Mair <lucy.may.mair@outlook.com>
2023-06-14 16:56:01 +0100

    Add console log message

--- svelte/src/main.ts
+++ svelte/src/main.ts
@@ -5,3 +5,17 @@ const app = new App({
 });
 
 export default app;
+
+const style = [
+  "color: mediumslateblue",
+  "font-family: 'Comic Sans MS'",
+  "font-size: 16px",
+  "line-height: 22px",
+].join(";");
+
+console.log(
+  "%cHey friend!\nI assume you're a fellow developer and you want to have a nose at my code. " +
+    "Luckily for you, this website is open source so you can see it in all its unminified glory " +
+    "at https://github.com/lucy-m/lucy-m-website.",
+  style
+);
