5d7eb7a7edb96f6b815c351a8a29eb4163e4c8f3
Lucy Mair <service+git@example.com>
2023-09-17 23:27:10 +0100

    Fix test

--- svelte/src/scenes/fishing/__tests__/fishing-scene.cy.ts
+++ svelte/src/scenes/fishing/__tests__/fishing-scene.cy.ts
@@ -6,7 +6,7 @@ describe("fishing scene", () => {
       cy.viewport(1400, 900);
 
       cy.mountViewScene({
-        initialSceneSpec: makeFishingScene({ kind: "cast-out" }),
+        initialSceneSpec: makeFishingScene({ kind: "idle" }),
         seed: "abcd",
       });
     });
