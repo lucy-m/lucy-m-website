6ca74a38c69126f836c8c00daf31e9c8404750f5
Lucy Mair <lucy.may.mair@outlook.com>
2023-10-09 22:56:19 +0100

    Add loading shimmer

--- svelte/src/scenes/fishing/overlays/NewFishCaught.svelte
+++ svelte/src/scenes/fishing/overlays/NewFishCaught.svelte
@@ -13,7 +13,9 @@
     <p>You caught a new fish!</p>
     <div class="fish-display">
       <p class="fish-name">{fishData.displayName}</p>
-      <img src={fishData.backgroundSrc} alt="" />
+      <div class="loading-container">
+        <img src={fishData.backgroundSrc} alt="" />
+      </div>
     </div>
     <button on:click={unmountSelf}>Cool!</button>
   </div>
@@ -26,8 +28,39 @@
     row-gap: 24px;
   }
 
+  .loading-container {
+    position: relative;
+    width: 100%;
+    padding-top: 100%;
+    background: hsl(210, 12%, 96%);
+    background-image: linear-gradient(
+      to right,
+      hsl(210, 12%, 96%) 0%,
+      hsl(225, 12%, 94%) 20%,
+      hsl(210, 12%, 96%) 40%,
+      hsl(210, 12%, 96%) 100%
+    );
+    background-size: 800px 100px;
+    animation-name: shimmer;
+    animation-duration: 1s;
+    animation-delay: 1s;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes shimmer {
+    0% {
+      background-position: -300px 0;
+    }
+
+    100% {
+      background-position: 300px 0;
+    }
+  }
+
   img {
     width: 100%;
+    position: absolute;
+    top: 0;
   }
 
   .fish-name {
