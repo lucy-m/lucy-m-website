b4040c62a6698de09d2f92919a62b0d1541816bc
Lucy Mair <lucy.may.mair@outlook.com>
2023-10-26 14:48:25 +0100

    Add new talk to section

--- svelte/src/components/Talks.svelte
+++ svelte/src/components/Talks.svelte
@@ -19,7 +19,9 @@
 
 <div class="talks">
   <div class="item-wrapper content-block">
-    <h2>Property-based testing</h2>
+    <div class="heading">
+      <h2>Property-based testing</h2>
+    </div>
     <ImagesAndText
       images={ndcLondonImgs}
       targetImageSize={400}
@@ -29,9 +31,38 @@
       ]}
     />
   </div>
+  <div class="item-wrapper content-block">
+    <div class="heading">
+      <h2>How to introduce new technology</h2>
+      <h3>and why you maybe shouldn't</h3>
+    </div>
+    <p>
+      This talk is still a bit of a work in progress and I will update later.
+    </p>
+    <p>
+      Picking up a new language or framework is always a fun side project, but
+      bringing these into production software at work is always a challenge. It
+      may not be compatible with other technologies you use. The project will
+      need to be maintained for possibly years to come. You will have to
+      convince the rest of your team it's a good idea and they should invest the
+      time to learn.
+    </p>
+    <p>
+      At Codat, we have used React for our front ends for years but earlier this
+      year we successfully did a project using Svelte. In this talk, I will
+      discuss why we decided to use Svelte, how to convince people to use a new
+      language or framework, and why you maybe shouldn’t.
+    </p>
+  </div>
 </div>
 
 <style>
+  .talks {
+    display: flex;
+    flex-direction: column;
+    row-gap: var(--spacing);
+  }
+
   .talks > :global(.item-wrapper) {
     /* This rule intentionally does nothing */
     box-sizing: border-box;
@@ -45,8 +76,16 @@
 
   h2 {
     font-family: "Bitter", "Quicksand", sans-serif;
-    font-weight: 700;
     font-size: min(2.2rem, 7vw);
+  }
+
+  h3 {
+    font-family: "Bitter", "Quicksand", sans-serif;
+    font-size: min(1.6rem, 4vw);
+    font-style: italic;
+  }
+
+  .heading {
     background-image: url("/underlines/underline_2.PNG");
     background-size: 400px;
     background-repeat: no-repeat;
