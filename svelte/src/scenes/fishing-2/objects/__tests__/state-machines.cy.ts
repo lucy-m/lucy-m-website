1c6251eed59d0d4b87df492bf918e7916a99dab2
Lucy Mair <service+git@example.com>
2025-01-31 18:30:20 +0000

    Add onPointerMove and concat

--- svelte/src/scenes/fishing-2/objects/__tests__/state-machines.cy.ts
+++ svelte/src/scenes/fishing-2/objects/__tests__/state-machines.cy.ts
@@ -1,53 +1,192 @@
 import { doTimes } from "../../../../utils";
-import { linearAnimation, type AnimatingState } from "../state-machines";
+import {
+  concatStates,
+  linearAnimation,
+  waitForPointerMove,
+  type AnimatingState,
+  type ObjectState,
+  type StateDone,
+  type WaitForPointerMoveState,
+} from "../state-machines";
 
-describe("linearAnimation", () => {
-  describe("from 4 to 7 over 6 ticks", () => {
-    let state: AnimatingState | undefined;
+const tickState = <TKind extends string, TState>(
+  lastState: ObjectState<TKind, TState> | StateDone
+) => {
+  if (lastState === "done") {
+    throw new Error("State is done");
+  }
+  if (!lastState || !lastState.tick) {
+    throw new Error("State does not have handler for 'tick'");
+  }
+  return lastState.tick();
+};
+
+const onPointerMoveState = <TKind extends string, TState>(
+  lastState: ObjectState<TKind, TState> | StateDone
+) => {
+  if (lastState === "done") {
+    throw new Error("State is done");
+  }
+  if (!lastState || !lastState.onPointerMove) {
+    throw new Error("State does not have handler for 'onPointerMove'");
+  }
+  return lastState.onPointerMove();
+};
+
+describe("state machines", () => {
+  describe("linearAnimation", () => {
+    describe("from 4 to 7 over 6 ticks", () => {
+      let lastState: AnimatingState | StateDone;
+
+      beforeEach(() => {
+        lastState = linearAnimation({
+          stepEnd: 6,
+          fromValue: 4,
+          toValue: 7,
+        });
+      });
+
+      it("current is 4", () => {
+        expect(lastState).to.haveOwnProperty("kind", "animating");
+        expect(lastState).to.haveOwnProperty("current", 4);
+      });
+
+      it("does not onPointerMove", () => {
+        expect(lastState).not.to.haveOwnProperty("onPointerMove");
+      });
+
+      describe("tick", () => {
+        beforeEach(() => {
+          lastState = tickState(lastState);
+        });
+
+        it("current is 4.5", () => {
+          expect(lastState).to.haveOwnProperty("kind", "animating");
+          expect(lastState).to.haveOwnProperty("current", 4.5);
+        });
+      });
+
+      describe("ticking to last state", () => {
+        beforeEach(() => {
+          doTimes(6, () => {
+            lastState = tickState(lastState);
+          });
+        });
+
+        it("current is 7", () => {
+          expect(lastState).to.haveOwnProperty("kind", "animating");
+          expect(lastState).to.haveOwnProperty("current", 7);
+        });
+
+        describe("ticking again", () => {
+          beforeEach(() => {
+            lastState = tickState(lastState);
+          });
+
+          it("state is undefined", () => {
+            expect(lastState).to.eq("done");
+          });
+        });
+      });
+    });
+  });
+
+  describe("waitForPointerMoveState", () => {
+    let lastState: WaitForPointerMoveState | StateDone;
 
     beforeEach(() => {
-      state = linearAnimation({
-        stepEnd: 6,
-        fromValue: 4,
-        toValue: 7,
-      });
+      lastState = waitForPointerMove();
     });
 
-    it("currentValue is 4", () => {
-      expect(state).to.haveOwnProperty("kind", "animating");
-      expect(state).to.haveOwnProperty("currentValue", 4);
+    it("has correct properties", () => {
+      expect(lastState).to.haveOwnProperty("kind", "wait-for-pom");
+      expect(lastState).not.to.haveOwnProperty("tick");
     });
 
-    describe("tick", () => {
+    describe("onPointerMove", () => {
       beforeEach(() => {
-        state = state?.tick();
+        lastState = onPointerMoveState(lastState);
       });
 
-      it("currentValue is 4.5", () => {
-        expect(state).to.haveOwnProperty("kind", "animating");
-        expect(state).to.haveOwnProperty("currentValue", 4.5);
+      it("is done", () => {
+        expect(lastState).to.equal("done");
+      });
+    });
+  });
+
+  describe("concat", () => {
+    describe("two animating states", () => {
+      const state1 = linearAnimation({
+        stepEnd: 1,
+        fromValue: 0,
+        toValue: 2,
+      });
+
+      const state2 = linearAnimation({
+        stepEnd: 2,
+        fromValue: 10,
+        toValue: 12,
+      });
+
+      const initial = concatStates(state1, state2);
+      let entries: number[];
+
+      beforeEach(() => {
+        entries = [];
+
+        let lastResult: AnimatingState | StateDone = initial;
+
+        for (let i = 0; i < 10; i++) {
+          if (lastResult === "done") {
+            break;
+          }
+          entries.push(lastResult.current);
+          lastResult = tickState(lastResult);
+        }
+
+        if (lastResult !== "done") {
+          throw new Error("State has not finished iterating");
+        }
+      });
+
+      it("has correct values", () => {
+        expect(entries).to.deep.equal([0, 2, 10, 11, 12]);
       });
     });
 
-    describe("ticking to last state", () => {
-      beforeEach(() => {
-        doTimes(6, () => {
-          state = state?.tick();
-        });
+    describe("wait-for-pom then animating", () => {
+      const state1 = waitForPointerMove();
+
+      const state2 = linearAnimation({
+        stepEnd: 1,
+        fromValue: 0,
+        toValue: 2,
       });
 
-      it("currentValue is 7", () => {
-        expect(state).to.haveOwnProperty("kind", "animating");
-        expect(state).to.haveOwnProperty("currentValue", 7);
+      const initial = concatStates(state1, state2);
+
+      it("has no tick", () => {
+        expect(initial.tick).to.be.undefined;
       });
 
-      describe("ticking again", () => {
+      describe("onPointerMove", () => {
+        let result: ReturnType<typeof onPointerMoveState>;
+
         beforeEach(() => {
-          state = state?.tick();
+          result = onPointerMoveState(initial);
         });
 
-        it("state is undefined", () => {
-          expect(state).to.be.undefined;
+        it("has animating state", () => {
+          expect(result).to.haveOwnProperty("kind", "animating");
+          expect(result).to.haveOwnProperty("current", 0);
+        });
+
+        it("has tick", () => {
+          expect(result).to.haveOwnProperty("tick");
+        });
+
+        it("has no pointerMove", () => {
+          expect(result).not.to.haveOwnProperty("onPointerMove");
         });
       });
     });
--- svelte/src/scenes/fishing-2/objects/state-machines.ts
+++ svelte/src/scenes/fishing-2/objects/state-machines.ts
@@ -1,10 +1,15 @@
 import { PosFns } from "../../../model";
 
-export type AnimatingState = {
-  kind: "animating";
-  tick: () => AnimatingState | undefined;
-  currentValue: number;
-};
+export type StateDone = "done";
+
+export type ObjectState<TKind extends string, TState> = Readonly<{
+  kind: TKind;
+  tick?: () => ObjectState<TKind, TState> | StateDone;
+  onPointerMove?: () => ObjectState<TKind, TState> | StateDone;
+  current: TState;
+}>;
+
+export type AnimatingState = ObjectState<"animating", number>;
 
 export const linearAnimation = (args: {
   stepEnd: number;
@@ -15,10 +20,12 @@ export const linearAnimation = (args: {
     const tick = () => {
       if (step < args.stepEnd) {
         return linearAnimationStepper(step + 1);
+      } else {
+        return "done";
       }
     };
 
-    const currentValue = PosFns.linearInterpolate(
+    const current = PosFns.linearInterpolate(
       PosFns.new(0, args.fromValue),
       PosFns.new(args.stepEnd, args.toValue),
       step
@@ -26,10 +33,61 @@ export const linearAnimation = (args: {
 
     return {
       kind: "animating",
-      currentValue,
+      current,
       tick,
     };
   };
 
   return linearAnimationStepper(0);
+};
+
+export type WaitForPointerMoveState = ObjectState<"wait-for-pom", undefined>;
+
+export const waitForPointerMove = (): WaitForPointerMoveState => {
+  return {
+    kind: "wait-for-pom",
+    current: undefined,
+    onPointerMove: () => "done",
+  };
+};
+
+/**
+ * Concats two states. When the first state emits `"done"` will move to state2
+ */
+export const concatStates = <
+  T1Kind extends string,
+  T2Kind extends string,
+  T1State,
+  T2State
+>(
+  state1: ObjectState<T1Kind, T1State>,
+  state2: ObjectState<T2Kind, T2State>
+): ObjectState<T1Kind | T2Kind, T1State | T2State> => {
+  const { tick, onPointerMove } = state1;
+
+  return {
+    kind: state1.kind,
+    tick: tick
+      ? () => {
+          const result = tick();
+          console.debug("Ticking state result", result);
+          if (result === "done") {
+            return state2;
+          } else {
+            return concatStates(result, state2);
+          }
+        }
+      : undefined,
+    onPointerMove: onPointerMove
+      ? () => {
+          const result = onPointerMove();
+          if (result === "done") {
+            return state2;
+          } else {
+            return concatStates(result, state2);
+          }
+        }
+      : undefined,
+    current: state1.current,
+  };
 };
